d461bf22b210b71c4baa7f671f64339b4ab9366b
Steven Milne <steven@digitaldelivery.co.uk>
2013-11-06 16:11:45 +0000

    five minute port

---
+++ _site/README.md
@@ -0,0 +1,4 @@
+webretreat
+==========
+
+Source code for participating in webretreat sessions. More details at webretreat.org, follow @webretreat on twitter.
---
+++ _site/js/base.js
@@ -0,0 +1,198 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>webretreat - intensive practice days for web designers</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Webretreat is a project aiming to give front end web developers a free, open, high quality practice workshop format.">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <script type="text/javascript"> 
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-1342935-39']);
+      _gaq.push(['_trackPageview']); 
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })(); 
+    </script>
+  </head>
+
+  <body>
+
+    <div class="navbar navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">webretreat.org</a>
+          <div class="nav-collapse collapse pull-right">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li> 
+            </ul>
+            
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      
+      <div class="row">
+        <div class="span8">
+          <div class="hero-unit">
+            <h1>web retreat</h1>
+            <p>This is the home for webretreat - a project aiming to give front end web developers a high quality practice workshop format.</p> 
+            <p><a class="btn" href="http://twitter.com/webretreat">Follow us on twitter</a> 
+              <a class="btn" href="http://eepurl.com/sql5j">Join the mailing list</a> </p>
+          </div>
+        </div>
+ 
+        <div class="span4">
+          <h2>Grab the code</h2>
+          <p>On the day of webretreat you need this code to start the day. You can grab the code  by cloning this repo on github:</p>
+          <p><a class="btn btn-success" href="https://github.com/stevenmilne/webretreat">To Github &raquo;</a></p>
+          <p>Or you can download a .zip archive here:</p>
+          <p><a class="btn btn-success" href="https://github.com/stevenmilne/webretreat/archive/master.zip">Download the .zip &raquo;</a></p>
+        </div>
+      </div>
+      <!-- Example row of columns -->
+      <div class="row" id="about">
+        <div class="span8">
+          <h2>Background</h2>
+          <p class="lead">Webretreat is a brand new project aiming to bring all the fun of <a href="http://coderetreat.org/">coderetreat</a> style full day practice days to general web development, particularly to front end development.</p>
+          <p>Our first event takes place in December 2012, following which we will update this site with more information. Meanwhile if you are interested in hosting a webretreat locally please get in touch below.</p> 
+          <p>Thankyou to the sponsors of the first <a href="http://coderetreatabz.eventbrite.co.uk/">webretreat</a>, <a href="http://www.codify.ltd.uk/">Codify</a> and <a href="http://fifthring.com">Fifth Ring</a>, and to the <a href="http://abdn.ac.uk">University of Aberdeen</a> and <a href="http://neo.com">Neo</a> for supporting the event.</p> 
+        </div>
+        <div class="span4">
+          <h2>Events</h2>
+          <p><a href="http://coderetreatabz.eventbrite.co.uk/">8th December 2012, <strong>Aberdeen</strong></a></p>
+          <p>The first event is being held alongside a coderetreat on global day of coderetreat in Aberdeen. This was booked out in under 48 hours. We'll blog here following the event with any lessons learned, things we would change, and general feedback.</p>
+          <p><a href="#">February 2013, <strong>Manchester</strong></a></p>
+          <p>We are in early planning for a webretreat in Manchester in February 2013, if you'd like to take part join the mailing list below and we'll add you to the priority list. Final date and venue TBC.</p>
+        </div> 
+      </div>
+ 
+      <hr>
+
+      <div id="contact" class="row">
+        <div class="span4">
+          <h2>Contact Us</h2>
+          <p>The core team is listed on this page, so feel free to contact us individually. There are a couple of ways to get in touch with 'the project' though:</p>
+          <p>On twitter : <a class="btn" href="http://twitter.com/webretreat">@webretreat</a></p>
+          <p>On github : <a class="btn" href="https://github.com/stevenmilne/webretreat/issues">issues</a></p> 
+          <p>On email : <a class="btn" href="mailto:webretreat@digitaldelivery.co.uk">email</a></p> 
+        </div>
+
+        <div class="span4"> 
+          <!-- Begin MailChimp Signup Form -->
+          <div id="mc_embed_signup">
+          <form action="http://webretreat.us6.list-manage.com/subscribe/post?u=78d10f04c535dbd23ba3573b5&amp;id=ef2050aabe" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <h2>Mailing list</h2>
+            <p>We won't spam you, we'll send occassional updates as the project progresses, and announcements of events as they open ticket availability.</p> 
+          <div class="mc-field-group">
+            <label for="mce-EMAIL">Email Address  <span class="asterisk">(required)</span>
+          </label>
+            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+          </div>
+            <div id="mce-responses" class="clear">
+              <div class="response" id="mce-error-response" style="display:none"></div>
+              <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-success"></div>
+          </form>
+          </div>
+          <p></p>
+          <!--End mc_embed_signup-->
+        </div>
+        
+        <div class="span4">
+          <h2>Join In</h2>
+          <p>We'd love your feedback, and we'd love you to run a webretreat near you to help us refine the format.</p>
+          <p>There aren't any proper guides to running a webretreat as yet, but any of the team below would be delighted to spend time explaining what we've learned so far.</p> 
+        </div>
+ 
+      </div>
+  
+      <hr>
+
+      <div class="row">
+        <div class="span4">
+          <h2>Team</h2>
+          <p>Webretreat is a project out of <a href="http://refreshaberdeen.org">Refresh Aberdeen</a> with three contributors.</p> 
+        </div> 
+        <div class="span8"> 
+          <p class="pull-right">
+            <a class="btn btn-mini" href="http://stevenmilne.com">website</a>  
+            <a class="btn btn-mini"  href="http://twitter.com/stevenmilne">twitter</a>
+          </p>
+          <h4>Steve Milne</h4>
+          <p>Steve has been a 'web guy' since '94 and leads Refresh Aberdeen, designed the original webretreat workshops. Steve has organised a number of coderetreats in Aberdeen, and trains web designers / developers as part of his day job as technical lead within a large marketing agency.</p> 
+
+          <p class="pull-right">
+            <a class="btn btn-mini"  href="http://ianisted.co.uk/">website</a>
+            <a class="btn btn-mini"  href="http://twitter.com/ianisted">twitter</a>
+          </p>
+          <h4>Ian Isted</h4>  
+          <p>Ian is facilitating the first webretreat in December 2012 in Aberdeen with Steve, and has been involved throughout the creation of the format and the creation of that awful source code. Ian works as a front end developer for a growing web agency based in Aberdeen.</p> 
+
+          <p class="pull-right">
+            <a class="btn btn-mini" href="http://about.me/mitchinator">website</a>
+            <a class="btn btn-mini" href="http://twitter.com/mitchinator">twitter</a>
+          </p>
+          <h4>Kevin Mitchell</h4>  
+          <p>A key contributor to the original project that led to web retreat, the Mitchinator builds web and mobile sites and apps all day long. In addition to his role at a specialist digital marketing agency, Kevin is also behind the news and information app <a href="http://furrybootsapp.com/">Furry boots</a>.</p> 
+         
+        </div> 
+      </div>
+ 
+      <footer>
+        <p>&copy; 2012, <a href="http://stevenmilne.com">Steve Milne</a>.</p> 
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
